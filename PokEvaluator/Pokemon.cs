bb7624d4b166f7ca04549995e5a9cfc39b97f55a
audouze <audouzed@gmail.com>
2017-08-07 10:20:13 +0200

    Lost Pokemon.cs by mistake

---
+++ PokEvaluator/Pokemon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PokEvaluator
+{
+    public class Pokemon
+    {
+        public String Name { get; set; }
+        public Element Element { get; set; }
+        public Element? Element2 { get; set; }
+        public PokemonType PokemonType { get;  private set; }
+
+        // constructeur par défaut pour désérialisation xml
+        public Pokemon()
+        {
+
+        }
+
+        public Pokemon(string name,Element mainElement,Element? secondaryElement = null)
+        {
+            Name = name;
+            Element = mainElement;
+            Element2 = secondaryElement;
+
+            PokemonType = Pokedex.PokemonTypes.Find(t => t.Element.Equals(mainElement) && t.Element2.Equals(secondaryElement));
+        }
+
+        public override string ToString(){ return Name; }
+    }
+}
